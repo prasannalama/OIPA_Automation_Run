32e5614af23f7fff87109a674a29bf2359f692a4
prasannalama <66576233+prasannalama@users.noreply.github.com>
2020-09-19 22:39:54 +0530

    Add files via upload

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,22 +1,43 @@
 package test.java;
 
+import java.sql.Connection;
+import java.util.Properties;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 
 public class NewTest {
 
+	public static WebDriver driver1;
+	public static String Browser;
+	public XSSFWorkbook wb;
+	public XSSFSheet sheet1;
+	Properties p;
+	public Connection con;
+	String connectionString;
 	@Test
 	public static void testgooglesearch() 
 	{
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://google.in");
-		// we expect the title â€œGoogle â€œ should be present
+		
+		
+		System.out.println("Launching browser..");
+		Browser = "chrome";
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+		options.addArguments("--silent");
+		driver1 = new ChromeDriver(options);
+		
+		
+		driver1.get("http://google.in");
+		// we expect the title “Google “ should be present
 		String Expectedtitle = "Google";
 		// it will fetch the actual title
-		String Actualtitle = driver.getTitle();
+		String Actualtitle = driver1.getTitle();
 		System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
 		// it will compare actual title and expected title
 		// print out the result
